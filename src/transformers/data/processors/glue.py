09ec38b4a7620addc2828f0fca3653c81217e04d
TobiasLee <tobiaslee@qq.com>
2020-04-24 16:15:57 +0800

    update processors for persona

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -158,6 +158,42 @@ class OutputMode(Enum):
     classification = "classification"
     regression = "regression"
 
+class PersonaProcessor(DataProcessor):
+    """Processor for the MRPC data set (GLUE version)."""
+
+    def get_example_from_tensor_dict(self, tensor_dict):
+        """See base class."""
+        return InputExample(
+            tensor_dict["idx"].numpy(),
+            tensor_dict["sentence"].numpy().decode("utf-8"),
+            None,
+            str(tensor_dict["label"].numpy()),
+        )
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        logger.info("LOOKING AT {}".format(os.path.join(data_dir, "train.co.tsv")))
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "train.co.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "test.co.tsv")), "tst")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = "<SEP>".join(line[1:])
+            label = line[0]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
 
 class MrpcProcessor(DataProcessor):
     """Processor for the MRPC data set (GLUE version)."""
@@ -519,6 +555,7 @@ glue_tasks_num_labels = {
     "qnli": 2,
     "rte": 2,
     "wnli": 2,
+    "persona": 2,
 }
 
 glue_processors = {
@@ -526,6 +563,7 @@ glue_processors = {
     "mnli": MnliProcessor,
     "mnli-mm": MnliMismatchedProcessor,
     "mrpc": MrpcProcessor,
+    "persona": PersonaProcessor, 
     "sst-2": Sst2Processor,
     "sts-b": StsbProcessor,
     "qqp": QqpProcessor,
@@ -539,6 +577,7 @@ glue_output_modes = {
     "mnli": "classification",
     "mnli-mm": "classification",
     "mrpc": "classification",
+    "persona": "classification", 
     "sst-2": "classification",
     "sts-b": "regression",
     "qqp": "classification",
