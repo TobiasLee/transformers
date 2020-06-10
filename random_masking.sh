d4ce790bd72a5b85287e2dbb72fc1742a630db58
Lei Li <tobiaslee@qq.com>
2020-06-10 20:02:26 +0800

    update script

--- random_masking.sh
+++ random_masking.sh
@@ -1,14 +1,14 @@
 TASK_NAME="qqp"
 TASK_DATA="QQP"
-HEAD_NUM=82
+HEAD_NUM=84
 DATA_DIR=glue_data
-GPU=1
+GPU=7
 for seed in 1234 2345 3456 4567 5678 # 1234 
 do
 CUDA_VISIBLE_DEVICES=$GPU python ./examples/bertology/run_bertology.py \
     --data_dir $DATA_DIR/$TASK_DATA --head_num $HEAD_NUM  --per_iter_mask 2 --model_name_or_path  "results/${TASK_NAME}_output_$seed" \
     --task_name $TASK_NAME --batch_size 32  \
     --max_seq_length 128 --cache_dir ~/.cache/torch/transformers \
-    --output_dir results/all_sequencial_variance_${TASK_NAME}_importance_score_head_num${HEAD_NUM}_output_$seed/ \
-    --try_masking
+    --output_dir results/random_mask_${TASK_NAME}_head_num${HEAD_NUM}_output_$seed/ \
+    --random_masking
 done
