1bc4ff4737700dfd64d05a95da06b7b08bd3e1d3
LiLei <tobiaslee@ralee.cc>
2020-07-10 19:15:49 +0800

    update

---
+++ run_mixed.sh
@@ -0,0 +1,18 @@
+export GLUE_DIR=glue_data
+GPU="2,3,4,5"
+for seed in 1234 #2345 3456 4567 5678
+do
+CUDA_VISIBLE_DEVICES=$GPU python run_glue_mixed.py \
+  --base_model_name_or_path bert-base-cased \
+  --large_model_name_or_path bert-large-cased \
+  --task_name mnli \
+  --do_train --save_total_limit 10 --save_steps 1000\
+  --do_eval \
+  --data_dir $GLUE_DIR/MNLI/ \
+  --max_seq_length 128 \
+  --per_gpu_train_batch_size 16 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3.0 \
+  --output_dir mnli_output_$seed/ \
+  --fp16 --seed $seed
+done
