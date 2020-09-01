f7ae756ca369804c292e7bfe92d80491fa82f34d
Lei Li <tobiaslee@qq.com>
2020-09-01 21:51:18 +0800

    fix

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -845,7 +845,7 @@ class Trainer:
 
         expected_saving = 1.0
         if len(paths) > 0:
-            print(paths[:20])
+            print(paths[:200])
             total_sum = 0.0
             for batch_path in paths:
                 total_sum += np.sum(batch_path.cpu().numpy())
