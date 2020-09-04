e7d0c875215bf95a2990cc519eefb6d58f4fbad9
LiLei <tobiaslee@ralee.cc>
2020-09-04 23:48:23 +0800

    mofify bash

--- el_baseline.sh
+++ el_baseline.sh
@@ -4,6 +4,7 @@ task="sst-2"
 task_data="SST-2"
 LOSS_TYPE="dl"
 DL_GMA=1.0
+BSZ=32
 for seed in 1234 2345 3456 #  4567 5678
 do
 CUDA_VISIBLE_DEVICES=$GPU python run_el_glue.py \
@@ -13,7 +14,7 @@ CUDA_VISIBLE_DEVICES=$GPU python run_el_glue.py \
   --do_eval --loss_type $LOSS_TYPE --dl_gamma $DL_GMA\
   --data_dir $GLUE_DIR/$task_data \
   --max_seq_length 128 \
-  --per_gpu_train_batch_size 16 \
+  --per_device_train_batch_size $BSZ \
   --learning_rate 2e-5 \
   --num_train_epochs 4.0 \
   --output_dir ${task}_output_${seed}_${LOSS_TYPE}_GMA${DL_GMA}/ \
