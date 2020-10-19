2c7aeeac2786c4bdf1d78b5fdb426d6b7a956961
TobiasLee <tobiaslee@qq.com>
2020-10-19 20:44:26 +0800

    update script

--- run_glue_nl.py
+++ run_glue_nl.py
@@ -185,24 +185,25 @@ def main():
 
     optimizer_grouped_parameters = None
     if model_args.only_train_classifier:
+        logger.info("only updating pooler & classifier parameters")
         no_decay = ["bias", "LayerNorm.weight"]
         # train classifier and pooler
         optimizer_grouped_parameters = [
                 {
-                    "params": [p for n, p in model.bert.classifier.named_parameters() if not any(nd in n for nd in no_decay)],
+                    "params": [p for n, p in model.classifier.named_parameters() if not any(nd in n for nd in no_decay)],
                     "weight_decay": training_args.weight_decay,
                 },
                 {
-                    "params": [p for n, p in model.bert.encoder.pooler.named_parameters() if
+                    "params": [p for n, p in model.bert.pooler.named_parameters() if
                                not any(nd in n for nd in no_decay)],
                     "weight_decay": training_args.weight_decay,
                 },
                 {
-                    "params": [p for n, p in model.bert.encoder.pooler.named_parameters() if any(nd in n for nd in no_decay)],
+                    "params": [p for n, p in model.bert.pooler.named_parameters() if any(nd in n for nd in no_decay)],
                     "weight_decay": 0.0,
                 },
                 {
-                    "params": [p for n, p in model.bert.classifier.named_parameters() if
+                    "params": [p for n, p in model.classifier.named_parameters() if
                                any(nd in n for nd in no_decay)],
                     "weight_decay": 0.0,
                 },
