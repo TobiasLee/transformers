118c1d77a7275dcb962859195894174157aac97f
TobiasLee <tobiaslee@qq.com>
2020-10-28 13:35:52 +0800

    fix

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -365,7 +365,7 @@ class TwentyNGProcessor(DataProcessor):
 class IMDBProcessor(DataProcessor):
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv.clean")), "dev")
 
     def get_train_examples(self, data_dir):
         """See base class."""
@@ -404,11 +404,11 @@ class Sst2Processor(DataProcessor):
 
     def get_dev_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.original.tsv")), "dev")
 
     def get_test_examples(self, data_dir):
         """See base class."""
-        return self._create_examples(self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+        return self._create_examples(self._read_tsv(os.path.join(data_dir, "dev.original.tsv")), "test")
 
     def get_labels(self):
         """See base class."""
